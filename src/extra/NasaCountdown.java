6eb300baada84ccfd7b4b91aa35e7d5c67a9d6e9
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-11-28 13:56:27 -0800

    WEEK 11

--- src/extra/NasaCountdown.java
+++ src/extra/NasaCountdown.java
@@ -17,20 +17,25 @@ import javax.swing.JOptionPane;
 public class NasaCountdown {
 	public static void main(String[] args) throws InterruptedException {
 		// 2. Use a pop-up to ask the user where to start counting from
-		String start = JOptionPane.showInputDialog("Where should we start counting from?");
+		 String decision= JOptionPane.showInputDialog("Where should we start counting from?");
 
 		// 3. Change the countdown to use the new starting point
-		System.out.println();
+		//int i = Integer.parseInt(decision);
 		
 		// 1. Print a countdown from 10 to 0 on the console
+		for (int i = Integer.parseInt(decision); i>=0; i--) {
+			//change string into int with integer.parseInt
 		
-		System.out.println();
-	
+		System.out.println(i);
+		speak(i+"");
+		}
+	//change int into string by +"
 			// 4. Use the speak method to hear the countdown.
 		
 			// 6. Use the following code to make the program wait one second for each number: Thread.sleep(1000);
-		
+		Thread.sleep(1000);
 		// 5. when the counting is done, speak "blastoff!"
+		speak("blastoff!");
 	}
 
 static void speak(String words) {
